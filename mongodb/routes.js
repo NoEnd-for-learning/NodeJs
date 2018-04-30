3b141b62ea506a18aff942a897a7b5e9850edf56
xrtShu <xrtshu@126.com>
2018-04-30 09:11:25 +0800

    t-0430 向服务器应用添加CRUD 操作

---
+++ mongodb/routes.js
@@ -0,0 +1,129 @@
+/*
+* routes.js -- module to provide routing
+* */
+
+'use strict'
+
+const mongodb = require('mongodb')
+
+const server = new mongodb.Server('localhost', 27017)
+
+const db = new mongodb.Db('spa', server)
+
+const makeMongoId = mongodb.ObjectID // 方便操作
+
+const configRoutes = (app, server) => {
+  app.get('/', (req, res) => {
+    res.redirect('/spa.html')
+  })
+
+  app.all('/:obj_type/*?', (req, res, next) => {
+    res.contentType('json')
+    next()
+  })
+
+  app.get('/:obj_type/list', (req, res) => {
+    db.collection(
+      req.params.obj_type,
+      function (outer_error, collection) {
+          collection.find().toArray(
+            (inner_error, map_list) => {
+              let data = {
+                code: '0000',
+                msg: 'hello world',
+                data: map_list,
+                success: true
+              }
+              res.send(data)
+            }
+          )
+      }
+    )
+  })
+
+  app.post('/:obj_type/create', (req, res) => {
+    db.collection(
+      req.params.obj_type,
+      function (outer_error, collection) {
+        let obj_map = req.body
+        collection.insert(
+          obj_map,
+          options.map,
+          (inner_error, result_map) => {
+            res.send(result_map)
+          }
+        )
+      }
+    )
+  })
+
+  app.get('/:obj_type/read/:id', (req, res) => {
+    let find_map = { _id: makeMongoId(req.params.id) }
+    db.collection(
+      req.params.obj_type,
+      function (outer_error, collection) { // findOne 方法，查找并返回匹配搜索参数的第一个文档
+        collection.findOne(
+          find_map,
+          (inner_error, result_map) => {
+            res.send(result_map)
+          }
+        )
+      }
+    )
+  })
+
+  app.post('/:obj_type/update/:id', (req, res) => {
+    let find_map = { _id: makeMongoId(req.params.id) }
+    let obj_map = req.body
+    db.collection(
+      req.params.obj_type,
+      function (outer_error, collection) {
+        let sort_order = []
+        let option_map = {
+          'new': true, upsert: false
+        }
+        collection.findAndModify(
+          find_map,
+          sort_order,
+          obj_map,
+          option_map,
+          (inner_error, updated_map) => {
+            res.send(updated_map)
+          }
+        )
+      }
+  )
+  })
+
+  app.get('/:obj_type/delete/:id', (req, res) => {
+    let find_map = { _id: makeMongoId(req.params.id) }
+    db.collection(
+      req.params.obj_type,
+      function (outer_error, collection) { // 使用remove 方法移除所有匹配对象映射属性的文档。传入single: true 选项，这样最多就只会删除一个文档。
+        let options_map = { single: true }
+        collection.remove(
+          find_map,
+          options_map,
+          (inner_error, delete_count) => {
+            let config = {
+              code: '0000',
+              data: null,
+              msg: delete_count.n === 1 ? '删除成功' : '删除数据为空',
+              success: delete_count.ok === 1 && delete_count.n === 1
+            }
+            res.send({ delete_count: config })
+          }
+        )
+      }
+    )
+  })
+}
+
+module.exports = { configRoutes: configRoutes }
+
+// -------- begin module initialization ---------
+db.open(() => {
+  console.log(`** Connected to MongoDB **`)
+})
+
+// -------- end module initialization ---------
